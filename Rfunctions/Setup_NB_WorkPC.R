20e057cce2f60020dc3560c4e27607f6d3fdbf3e
Lionel Leston <leston@ualberta.ca>
2018-09-12 02:22:15 -0600

    Data downloaded from BAM drive on Sep. 11, 2018
    5 folders: images, rawData, RDataFiles, Rfunctions, RmdStyles

---
+++ Rfunctions/Setup_NB_WorkPC.R
@@ -0,0 +1,12 @@
+#Personalized setup to run at beginning of each script
+
+
+# For Nicole's work PC, here are the two directories I care about
+
+# set Dropbox directory
+droproot <- "C:/Users/nbarker/Dropbox/BAM/BC_Risk_Matrix/"
+
+# set repos directory
+reposroot <- "C:/Users/nbarker/Documents/repos/"
+
+
